4e3a9a04109f2211448b98652dec4b1590c0a0f7
Barkers92 <38814056+Barkers92@users.noreply.github.com>
2020-05-25 13:32:47 +0100

    Initial commit

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN1 = new System.Windows.Forms.Button();
+            this.BTN2 = new System.Windows.Forms.Button();
+            this.BTN3 = new System.Windows.Forms.Button();
+            this.BTN4 = new System.Windows.Forms.Button();
+            this.BTN5 = new System.Windows.Forms.Button();
+            this.BTN6 = new System.Windows.Forms.Button();
+            this.BTN7 = new System.Windows.Forms.Button();
+            this.BTN8 = new System.Windows.Forms.Button();
+            this.BTN9 = new System.Windows.Forms.Button();
+            this.BTN0 = new System.Windows.Forms.Button();
+            this.TBEquationView = new System.Windows.Forms.TextBox();
+            this.BTNMinus = new System.Windows.Forms.Button();
+            this.BTNPlus = new System.Windows.Forms.Button();
+            this.BTNMultiply = new System.Windows.Forms.Button();
+            this.BTNDivide = new System.Windows.Forms.Button();
+            this.BTNEquals = new System.Windows.Forms.Button();
+            this.BTNDecimal = new System.Windows.Forms.Button();
+            this.BTNClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN1
+            // 
+            this.BTN1.Location = new System.Drawing.Point(84, 107);
+            this.BTN1.Name = "BTN1";
+            this.BTN1.Size = new System.Drawing.Size(76, 76);
+            this.BTN1.TabIndex = 0;
+            this.BTN1.Text = "1";
+            this.BTN1.UseVisualStyleBackColor = true;
+            this.BTN1.Click += new System.EventHandler(this.BTN1_Click);
+            // 
+            // BTN2
+            // 
+            this.BTN2.Location = new System.Drawing.Point(166, 107);
+            this.BTN2.Name = "BTN2";
+            this.BTN2.Size = new System.Drawing.Size(76, 76);
+            this.BTN2.TabIndex = 1;
+            this.BTN2.Text = "2";
+            this.BTN2.UseVisualStyleBackColor = true;
+            this.BTN2.Click += new System.EventHandler(this.BTN2_Click);
+            // 
+            // BTN3
+            // 
+            this.BTN3.Location = new System.Drawing.Point(248, 107);
+            this.BTN3.Name = "BTN3";
+            this.BTN3.Size = new System.Drawing.Size(76, 76);
+            this.BTN3.TabIndex = 2;
+            this.BTN3.Text = "3";
+            this.BTN3.UseVisualStyleBackColor = true;
+            this.BTN3.Click += new System.EventHandler(this.BTN3_Click);
+            // 
+            // BTN4
+            // 
+            this.BTN4.Location = new System.Drawing.Point(84, 189);
+            this.BTN4.Name = "BTN4";
+            this.BTN4.Size = new System.Drawing.Size(76, 76);
+            this.BTN4.TabIndex = 3;
+            this.BTN4.Text = "4";
+            this.BTN4.UseVisualStyleBackColor = true;
+            this.BTN4.Click += new System.EventHandler(this.BTN4_Click);
+            // 
+            // BTN5
+            // 
+            this.BTN5.Location = new System.Drawing.Point(166, 189);
+            this.BTN5.Name = "BTN5";
+            this.BTN5.Size = new System.Drawing.Size(76, 76);
+            this.BTN5.TabIndex = 4;
+            this.BTN5.Text = "5";
+            this.BTN5.UseVisualStyleBackColor = true;
+            this.BTN5.Click += new System.EventHandler(this.BTN5_Click);
+            // 
+            // BTN6
+            // 
+            this.BTN6.Location = new System.Drawing.Point(248, 189);
+            this.BTN6.Name = "BTN6";
+            this.BTN6.Size = new System.Drawing.Size(76, 76);
+            this.BTN6.TabIndex = 5;
+            this.BTN6.Text = "6";
+            this.BTN6.UseVisualStyleBackColor = true;
+            this.BTN6.Click += new System.EventHandler(this.BTN6_Click);
+            // 
+            // BTN7
+            // 
+            this.BTN7.Location = new System.Drawing.Point(84, 271);
+            this.BTN7.Name = "BTN7";
+            this.BTN7.Size = new System.Drawing.Size(76, 76);
+            this.BTN7.TabIndex = 6;
+            this.BTN7.Text = "7";
+            this.BTN7.UseVisualStyleBackColor = true;
+            this.BTN7.Click += new System.EventHandler(this.BTN7_Click);
+            // 
+            // BTN8
+            // 
+            this.BTN8.Location = new System.Drawing.Point(166, 271);
+            this.BTN8.Name = "BTN8";
+            this.BTN8.Size = new System.Drawing.Size(76, 76);
+            this.BTN8.TabIndex = 7;
+            this.BTN8.Text = "8";
+            this.BTN8.UseVisualStyleBackColor = true;
+            this.BTN8.Click += new System.EventHandler(this.BTN8_Click);
+            // 
+            // BTN9
+            // 
+            this.BTN9.Location = new System.Drawing.Point(248, 271);
+            this.BTN9.Name = "BTN9";
+            this.BTN9.Size = new System.Drawing.Size(76, 76);
+            this.BTN9.TabIndex = 8;
+            this.BTN9.Text = "9";
+            this.BTN9.UseVisualStyleBackColor = true;
+            this.BTN9.Click += new System.EventHandler(this.BTN9_Click);
+            // 
+            // BTN0
+            // 
+            this.BTN0.Location = new System.Drawing.Point(166, 353);
+            this.BTN0.Name = "BTN0";
+            this.BTN0.Size = new System.Drawing.Size(76, 76);
+            this.BTN0.TabIndex = 9;
+            this.BTN0.Text = "0";
+            this.BTN0.UseVisualStyleBackColor = true;
+            this.BTN0.Click += new System.EventHandler(this.BTN0_Click);
+            // 
+            // TBEquationView
+            // 
+            this.TBEquationView.Location = new System.Drawing.Point(84, 51);
+            this.TBEquationView.Name = "TBEquationView";
+            this.TBEquationView.Size = new System.Drawing.Size(322, 26);
+            this.TBEquationView.TabIndex = 10;
+            // 
+            // BTNMinus
+            // 
+            this.BTNMinus.Location = new System.Drawing.Point(330, 271);
+            this.BTNMinus.Name = "BTNMinus";
+            this.BTNMinus.Size = new System.Drawing.Size(76, 76);
+            this.BTNMinus.TabIndex = 11;
+            this.BTNMinus.Text = "-";
+            this.BTNMinus.UseVisualStyleBackColor = true;
+            this.BTNMinus.Click += new System.EventHandler(this.BTNMinus_Click);
+            // 
+            // BTNPlus
+            // 
+            this.BTNPlus.Location = new System.Drawing.Point(330, 353);
+            this.BTNPlus.Name = "BTNPlus";
+            this.BTNPlus.Size = new System.Drawing.Size(76, 76);
+            this.BTNPlus.TabIndex = 12;
+            this.BTNPlus.Text = "+";
+            this.BTNPlus.UseVisualStyleBackColor = true;
+            this.BTNPlus.Click += new System.EventHandler(this.BTNPlus_Click);
+            // 
+            // BTNMultiply
+            // 
+            this.BTNMultiply.Location = new System.Drawing.Point(330, 189);
+            this.BTNMultiply.Name = "BTNMultiply";
+            this.BTNMultiply.Size = new System.Drawing.Size(76, 76);
+            this.BTNMultiply.TabIndex = 13;
+            this.BTNMultiply.Text = "*";
+            this.BTNMultiply.UseVisualStyleBackColor = true;
+            this.BTNMultiply.Click += new System.EventHandler(this.BTNMultiply_Click);
+            // 
+            // BTNDivide
+            // 
+            this.BTNDivide.Location = new System.Drawing.Point(330, 107);
+            this.BTNDivide.Name = "BTNDivide";
+            this.BTNDivide.Size = new System.Drawing.Size(76, 76);
+            this.BTNDivide.TabIndex = 14;
+            this.BTNDivide.Text = "/";
+            this.BTNDivide.UseVisualStyleBackColor = true;
+            this.BTNDivide.Click += new System.EventHandler(this.BTNDivide_Click);
+            // 
+            // BTNEquals
+            // 
+            this.BTNEquals.Location = new System.Drawing.Point(166, 435);
+            this.BTNEquals.Name = "BTNEquals";
+            this.BTNEquals.Size = new System.Drawing.Size(158, 76);
+            this.BTNEquals.TabIndex = 15;
+            this.BTNEquals.Text = "=";
+            this.BTNEquals.UseVisualStyleBackColor = true;
+            this.BTNEquals.Click += new System.EventHandler(this.BTNEquals_Click);
+            // 
+            // BTNDecimal
+            // 
+            this.BTNDecimal.Location = new System.Drawing.Point(84, 353);
+            this.BTNDecimal.Name = "BTNDecimal";
+            this.BTNDecimal.Size = new System.Drawing.Size(76, 76);
+            this.BTNDecimal.TabIndex = 16;
+            this.BTNDecimal.Text = ".";
+            this.BTNDecimal.UseVisualStyleBackColor = true;
+            this.BTNDecimal.Click += new System.EventHandler(this.BTNDecimal_Click);
+            // 
+            // BTNClear
+            // 
+            this.BTNClear.Location = new System.Drawing.Point(248, 353);
+            this.BTNClear.Name = "BTNClear";
+            this.BTNClear.Size = new System.Drawing.Size(76, 76);
+            this.BTNClear.TabIndex = 17;
+            this.BTNClear.Text = "C";
+            this.BTNClear.UseVisualStyleBackColor = true;
+            this.BTNClear.Click += new System.EventHandler(this.BTNClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 551);
+            this.Controls.Add(this.BTNClear);
+            this.Controls.Add(this.BTNDecimal);
+            this.Controls.Add(this.BTNEquals);
+            this.Controls.Add(this.BTNDivide);
+            this.Controls.Add(this.BTNMultiply);
+            this.Controls.Add(this.BTNPlus);
+            this.Controls.Add(this.BTNMinus);
+            this.Controls.Add(this.TBEquationView);
+            this.Controls.Add(this.BTN0);
+            this.Controls.Add(this.BTN9);
+            this.Controls.Add(this.BTN8);
+            this.Controls.Add(this.BTN7);
+            this.Controls.Add(this.BTN6);
+            this.Controls.Add(this.BTN5);
+            this.Controls.Add(this.BTN4);
+            this.Controls.Add(this.BTN3);
+            this.Controls.Add(this.BTN2);
+            this.Controls.Add(this.BTN1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN1;
+        private System.Windows.Forms.Button BTN2;
+        private System.Windows.Forms.Button BTN3;
+        private System.Windows.Forms.Button BTN4;
+        private System.Windows.Forms.Button BTN5;
+        private System.Windows.Forms.Button BTN6;
+        private System.Windows.Forms.Button BTN7;
+        private System.Windows.Forms.Button BTN8;
+        private System.Windows.Forms.Button BTN9;
+        private System.Windows.Forms.Button BTN0;
+        private System.Windows.Forms.TextBox TBEquationView;
+        private System.Windows.Forms.Button BTNMinus;
+        private System.Windows.Forms.Button BTNPlus;
+        private System.Windows.Forms.Button BTNMultiply;
+        private System.Windows.Forms.Button BTNDivide;
+        private System.Windows.Forms.Button BTNEquals;
+        private System.Windows.Forms.Button BTNDecimal;
+        private System.Windows.Forms.Button BTNClear;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        //initialise the public variables in use
+        public string equation = "";
+        public double first_value;
+        public double second_value;
+        public string Operator;
+        public double result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //onclick functions for the numerical buttons
+        private void BTN1_Click(object sender, EventArgs e)
+        {
+            equation = equation + "1";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN2_Click(object sender, EventArgs e)
+        {
+            equation = equation + "2";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN3_Click(object sender, EventArgs e)
+        {
+            equation = equation + "3";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN4_Click(object sender, EventArgs e)
+        {
+            equation = equation + "4";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN5_Click(object sender, EventArgs e)
+        {
+            equation = equation + "5";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN6_Click(object sender, EventArgs e)
+        {
+            equation = equation + "6";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN7_Click(object sender, EventArgs e)
+        {
+            equation = equation + "7";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN8_Click(object sender, EventArgs e)
+        {
+            equation = equation + "8";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN9_Click(object sender, EventArgs e)
+        {
+            equation = equation + "9";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTN0_Click(object sender, EventArgs e)
+        {
+            equation = equation + "0";
+            TBEquationView.Text = equation;
+        }
+
+
+
+        //onclick functions for the operators
+        private void BTNDivide_Click(object sender, EventArgs e)
+        {
+            if (TBEquationView.Text == null || TBEquationView.Text == "")
+            {
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            if (Operator == null || Operator == "")
+            {
+                first_value = System.Convert.ToDouble(TBEquationView.Text);
+                Operator = "/";
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            else if (Operator == "+")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value + second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "-")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value - second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "/")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value / second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "*")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value * second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+        }
+
+        private void BTNMultiply_Click(object sender, EventArgs e)
+        {
+            if (TBEquationView.Text == null || TBEquationView.Text == "")
+            {
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            if (Operator == null || Operator == "")
+            {
+                first_value = System.Convert.ToDouble(TBEquationView.Text);
+                Operator = "*";
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            else if (Operator == "+")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value + second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "-")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value - second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "/")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value / second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "*")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value * second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+        }
+
+        private void BTNMinus_Click(object sender, EventArgs e)
+        {
+            if (TBEquationView.Text == null || TBEquationView.Text == "")
+            {
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            if (Operator == null || Operator == "")
+            {
+                first_value = System.Convert.ToDouble(TBEquationView.Text);
+                Operator = "-";
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            else if (Operator == "+")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value + second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "-")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value - second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "/")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value / second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "*")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value * second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+        }
+
+        private void BTNPlus_Click(object sender, EventArgs e)
+        {
+
+            if (TBEquationView.Text == null || TBEquationView.Text == "")
+            {
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            else if (Operator == null || Operator == "")
+            {
+                first_value = System.Convert.ToDouble(TBEquationView.Text);
+                Operator = "+";
+                TBEquationView.Text = "";
+                equation = "";
+            }
+            else if (Operator == "+")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value + second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "-")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value - second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "/")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value / second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+            else if (Operator == "*")
+            {
+                second_value = Convert.ToDouble(equation);
+                result = first_value * second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = result;
+                equation = "";
+            }
+        }
+
+        private void BTNDecimal_Click(object sender, EventArgs e)
+        {
+            equation = equation + ".";
+            TBEquationView.Text = equation;
+        }
+
+        private void BTNClear_Click(object sender, EventArgs e)
+        {
+            TBEquationView.Text = "";
+            first_value = 0;
+            second_value = 0;
+            Operator = "";
+            result = 0;
+        }
+
+        private void BTNEquals_Click(object sender, EventArgs e)
+        {
+            second_value = System.Convert.ToDouble(TBEquationView.Text);
+
+            if(Operator == "+")
+            {
+                result = first_value + second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = 0;
+                second_value = 0;
+                Operator = "";
+                result = 0;
+            }
+            if (Operator == "-")
+            {
+                result = first_value - second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = 0;
+                second_value = 0;
+                Operator = "";
+                result = 0;
+            }
+            if (Operator == "/")
+            {
+                result = first_value / second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = 0;
+                second_value = 0;
+                Operator = "";
+                result = 0;
+            }
+            if (Operator == "*")
+            {
+                result = first_value * second_value;
+                TBEquationView.Text = Convert.ToString(result);
+                first_value = 0;
+                second_value = 0;
+                Operator = "";
+                result = 0;
+            }
+        }
+    }
+}
